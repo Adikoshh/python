f863e788b8b7a845f73944ccd402012bd2964a34
Adikoshh <96627415+Adikoshh@users.noreply.github.com>
2022-09-27 22:42:06 +0600

    Update task 1.1.py

--- week4/task 1.1.py
+++ week4/task 1.1.py
@@ -3,16 +3,15 @@ a=[]
 for i in range(n):
     b=[]
     for j in range(n):
-        print('Введите [',i,',',j,'] элемент')
+        print('Enter the element:')
         b.append(int(input()))
     a.append(b)
-p = 0
-s = 0
+c = 0
+d = 0
 for i in range(n):
     for j in range(i + 1, n):
         if a[i][j] > 0:
-            s += a[i][j]
-            p+= 1
-
-print("number of positive:",p)
-print("sum:",s)
+            d += a[i][j]
+            c+= 1
+print("Number of positive: " , c)
+print("Sum: " , d)

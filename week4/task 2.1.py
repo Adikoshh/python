271f21ab0c99e4a43d97fb9f1004fbfb6ef052e2
Adikoshh <96627415+Adikoshh@users.noreply.github.com>
2022-09-27 22:43:44 +0600

    Update task 2.1.py

--- week4/task 2.1.py
+++ week4/task 2.1.py
@@ -3,17 +3,17 @@ a=[]
 for i in range(n):
     b=[]
     for j in range(n):
-        print('Введите [',i,',',j,'] элемент')
+        print('Enter the element: ')
         b.append(int(input()))
     a.append(b)
-def magsquare(a):
+def magic(a):
     n = 3
-    diag1 = 0
-    diag2 = 0
+    d1 = 0
+    d2 = 0
     for i in range(n):
-        diag1 += a[i][i]
-        diag2 += a[i][n - i - 1]
-    if not (diag1 == diag2):
+        d1 += a[i][i]
+        d2 += a[i][n - i - 1]
+    if not (d1 == d2):
         return False
     for i in range(n):
         rows = 0
@@ -21,12 +21,12 @@ def magsquare(a):
         for j in range(n):
             rows  += a[i][j]
             cols += a[j][i]
-        if not (rows  == cols == diag1):
+        if not (rows  == cols == d1):
             return False
 
     return True
 
-if (magsquare(a)):
+if (magic(a)):
     print("Magic Square")
 else:
     print("Not a magic Square")
